6eaeeb0e0cb35dec63b40219369d7fdef481da27
Lukas Laudrain <lukas.ldrn@gmail.com>
2025-01-27 11:09:48 +0100

    feat: page name

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from 'next';
+
 import * as fs from 'node:fs';
 
 import SubjectsPage from '@/feature/subjects/subjects-page';
+
+export const metadata: Metadata = {
+  title: 'Vilnius Tech Schedule Generator',
+};
 
 const Page = () => {
   const filepath = './src/constants/subjects.json';
